ff5863d7509708d426a3c7bf7d186e45d2e24911
Emmanuel Eliason-Armstrong <geteliarms@gmail.com>
2022-01-25 00:25:58 -0500

    Initial Commit

---
+++ downloadassets.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+
+if [[ -z "$INPUT_GITHUB_TOKEN" ]]; then
+  echo "Missing GITHUB TOKEN in the action"
+  exit 1
+fi
+
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Missing GITHUB_REPOSITORY env variable"
+  exit 1
+fi
+
+OWNER_REPOSITORY=$GITHUB_REPOSITORY
+if ! [[ -z ${INPUT_REPOSITORY} ]]; then
+  OWNER_REPOSITORY=$INPUT_REPOSITORY;
+fi
+
+
+                        
+AUTH="Authorization: token ${INPUT_GITHUB_TOKEN}"
+response=$(curl -sH "$AUTH" https://api.github.com/repos/${OWNER_REPOSITORY}/releases/${INPUT_RELEASE})
+id=`echo "$response" | jq '.assets[0] .id' |  tr -d '"'`
+name=`echo "$response" | jq '.assets[0] .name' |  tr -d '"'`
+GH_ASSET="https://api.github.com/repos/${OWNER_REPOSITORY}/releases/assets/$id"
+# download the artifact     
+curl -v -L -o "$name" -H "$AUTH" -H 'Accept: application/octet-stream' "$GH_ASSET"
+
+echo "::set-output name=artifact_name::success"
+echo "::set-output name=result::success"
